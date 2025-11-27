a2f0796b0f875bf3bbb77bff973aaa03beb5f27e
Sergio Araya Astudillo <ser.arayaa@duocuc.cl>
2025-11-27 00:44:39 -0300

    Delete mil-sabores-backend-fullstack2/src/main/java/cl/duoc/pasteleria/milsaboresbackendfullstack2 directory

--- mil-sabores-backend-fullstack2/src/main/java/cl/duoc/pasteleria/milsaboresbackendfullstack2/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.pasteleria.milsaboresbackendfullstack2.repository;
-
-import cl.duoc.pasteleria.milsaboresbackendfullstack2.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
